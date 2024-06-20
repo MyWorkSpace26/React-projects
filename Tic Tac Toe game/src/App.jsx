c16eb9515491ac0b3adffde5d6d5f5ae812a1a76
Todo-List-Project <103925523+MyWorkSpace26@users.noreply.github.com>
2024-06-20 11:29:25 +0300

    first commit

---
+++ Tic Tac Toe game/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player-name">player 1</span>
+            <span className="player-symbol">X</span>
+          </li>
+          <li>
+            <span className="player-name">player 2</span>
+            <span className="player-symbol">O</span>
+          </li>
+        </ol>
+      </div>
+    </main>
+  );
+}
+
+export default App;
