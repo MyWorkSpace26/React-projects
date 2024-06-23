4f67d028a5b29852e7d436c0dc2a35dc0dbf17e6
Todo-List-Project <103925523+MyWorkSpace26@users.noreply.github.com>
2024-06-23 13:00:04 +0300

    Updating State Based On Old State

--- Tic Tac Toe game/Components/Player.jsx
+++ Tic Tac Toe game/Components/Player.jsx
@@ -1,13 +1,24 @@
 import React from "react";
+import { useState } from "react";
 
 const Player = ({ name, symbol }) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleEditClick = () => {
+    setIsEditing((prevstate) => !prevstate);
+  };
+  let playerName = <span className="player-name">{name}</span>;
+  if (isEditing) {
+    playerName = <input type="text" required value={name} />;
+  }
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 };
--- Tic Tac Toe game/src/App.jsx
+++ Tic Tac Toe game/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import "./App.css";
 import Player from "../Components/Player";
 
