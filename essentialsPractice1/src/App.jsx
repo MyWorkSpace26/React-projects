dda39220637af95ea9265cff7812d85c10e93161
Todo-List-Project <103925523+MyWorkSpace26@users.noreply.github.com>
2024-07-01 14:54:18 +0300

    Adding a Header Component and Getting Started with a User Input Component

--- essentialsPractice1/src/App.jsx
+++ essentialsPractice1/src/App.jsx
@@ -1,38 +1,12 @@
-import { useState } from "react";
 import "./App.css";
+import Header from "./components/Header";
+import UserInput from "./components/UserInput";
 
-import logo from "../public/investment-calculator-logo.png";
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <header id="header">
-        <img src={logo} alt="Logo showing a money bag" />
-        <h1>Investment Calculator</h1>
-      </header>
-      <section id="user-input">
-        <div className="input-group">
-          <p>
-            <label htmlFor="initial-investment">Initial Investment</label>
-            <input type="number" required />
-          </p>
-          <p>
-            <label htmlFor="Annual Investmen">Annual Investment</label>
-            <input type="number" required />
-          </p>
-        </div>
-        <div className="input-group">
-          <p>
-            <label htmlFor="Expected Return">Expected Return</label>
-            <input type="number" required />
-          </p>
-          <p>
-            <label htmlFor="Duration">Duration</label>
-            <input type="number" required />
-          </p>
-        </div>
-      </section>
+      <Header />
+      <UserInput />
     </>
   );
 }
