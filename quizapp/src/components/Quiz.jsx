e2fc44a20abd5315a989b7357c90b07842a58f36
Todo-List-Project <103925523+MyWorkSpace26@users.noreply.github.com>
2024-08-05 16:55:45 +0300

    Moving Logic To Components && Setting Different Timers Based On The Selected Answer

--- quizapp/src/components/Quiz.jsx
+++ quizapp/src/components/Quiz.jsx
@@ -1,8 +1,8 @@
 import { useState, useCallback } from "react";
 
 import QUESTIONS from "../questions.js";
-import quizCompleteImg from "../assets/quiz-complete.png";
 import Question from "./Question.jsx";
+import Summary from "./Summary.jsx";
 const Quiz = () => {
   const [userAnswers, setUserAnswers] = useState([]);
 
@@ -24,12 +24,7 @@ const Quiz = () => {
   );
 
   if (quizIsComplete) {
-    return (
-      <div id="summary">
-        <img src={quizCompleteImg} alt="Trophy icon" />
-        <h2>Quiz Completed!</h2>
-      </div>
-    );
+    return <Summary userAnswers={userAnswers} />;
   }
 
   return (
