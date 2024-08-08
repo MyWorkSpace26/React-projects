89519f5a17f5cedc34c2a766672a1ff090cbe60d
Todo-List-Project <103925523+MyWorkSpace26@users.noreply.github.com>
2024-08-08 10:37:10 +0300

    adding the counter

--- counterproject/src/App.jsx
+++ counterproject/src/App.jsx
@@ -2,10 +2,29 @@ import { useState } from "react";
 import Header from "./components/Header";
 
 function App() {
+  console.log("<App /> rendered");
+
+  const [enteredNumber, setEnteredNumber] = useState(0);
+  const [chosenCount, setChosenCount] = useState(0);
+
+  function handleChange(event) {
+    setEnteredNumber(+event.target.value);
+  }
+
+  function handleSetClick() {
+    setChosenCount(enteredNumber);
+    setEnteredNumber(0);
+  }
   return (
     <>
       <Header />
-      <h1>Vite + React</h1>
+      <main>
+        <section id="configure-counter">
+          <h2>Set Counter</h2>
+          <input type="number" onChange={handleChange} value={enteredNumber} />
+          <button onClick={handleSetClick}>Set</button>
+        </section>
+      </main>
     </>
   );
 }
