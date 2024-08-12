4e04e2740045c6d962b7d09155d47b5711cffcd4
Todo-List-Project <103925523+MyWorkSpace26@users.noreply.github.com>
2024-08-12 12:33:05 +0300

    Handling HTTP Errors && Handling Loading States

--- placepicker/src/App.jsx
+++ placepicker/src/App.jsx
@@ -5,20 +5,39 @@ import Modal from "./components/Modal.jsx";
 import DeleteConfirmation from "./components/DeleteConfirmation.jsx";
 import logoImg from "./assets/logo.png";
 import { sortPlacesByDistance } from "./loc.js";
+import Error from "./components/Error.jsx";
 
 function App() {
   const selectedPlace = useRef();
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
+
   const [pickedPlaces, setPickedPlaces] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState();
 
   //fetch available Places from backend API
 
   useEffect(() => {
     async function fetchPlaces() {
-      const response = await fetch("http://localhost:3000/places");
-      const resData = await response.json();
-      setAvailablePlaces(resData.places);
+      setIsFetching(true);
+
+      try {
+        const response = await fetch("http://localhost:3000/placessss");
+        const resData = await response.json();
+        if (!response.ok) {
+          throw new Error("Faild to fetch data");
+        }
+        setAvailablePlaces(resData.places);
+      } catch (error) {
+        setError({
+          message:
+            error.message ||
+            "Could not fetch places , please try againg later.",
+        });
+      }
+
+      setIsFetching(false);
     }
     fetchPlaces();
   }, []);
@@ -80,12 +99,17 @@ function App() {
           places={pickedPlaces}
           onSelectPlace={handleStartRemovePlace}
         />
-        <Places
-          title="Available Places"
-          places={availablePlaces}
-          fallbackText="Sorting places by distance..."
-          onSelectPlace={handleSelectPlace}
-        />
+        {error && <Error title="An error occurred" message={error.message} />}
+        {!error && (
+          <Places
+            title="Available Places"
+            isLoading={isFetching}
+            loadingText="Fetching Places data..."
+            places={availablePlaces}
+            fallbackText="Sorting places by distance..."
+            onSelectPlace={handleSelectPlace}
+          />
+        )}
       </main>
     </>
   );
