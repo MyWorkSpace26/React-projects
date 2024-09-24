794fe1c9161fd9036e3d65541bb7d0ae25e3ed5e
Todo-List-Project <103925523+MyWorkSpace26@users.noreply.github.com>
2024-09-24 17:07:22 +0300

    Attaching Payloads to Actions

--- reduxproject/src/components/Counter.jsx
+++ reduxproject/src/components/Counter.jsx
@@ -10,6 +10,10 @@ const Counter = () => {
     dispatch({ type: "increment" });
   };
 
+  const increasetHandler = () => {
+    dispatch({ type: "increase", amount: 5 });
+  };
+
   const decrementHandler = () => {
     dispatch({ type: "decrement" });
   };
@@ -22,6 +26,7 @@ const Counter = () => {
       <div className={classes.value}>{counter}</div>
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increasetHandler}>Increase</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
