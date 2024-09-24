8d488cb4192efcdfdfcc98ce4d833369767f2096
Todo-List-Project <103925523+MyWorkSpace26@users.noreply.github.com>
2024-09-24 16:57:39 +0300

    Dispatching Actions From Inside Components

--- reduxproject/src/store/index.js
+++ reduxproject/src/store/index.js
@@ -2,10 +2,10 @@ import { createStore } from "redux";
 
 const counterReducer = (state = { counter: 0 }, action) => {
   switch (action.type) {
-    case "INCREMENT":
-      return (counter = state.counter + 1);
-    case "DECREMENT":
-      return (counter = state.counter - 1);
+    case "increment":
+      return { counter: state.counter + 1 };
+    case "decrement":
+      return { counter: state.counter - 1 };
     default:
       return state;
   }
