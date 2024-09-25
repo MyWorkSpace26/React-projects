6d29ce42ed57c1f73fe1fee414fd9a0a3432fdb7
Todo-List-Project <103925523+MyWorkSpace26@users.noreply.github.com>
2024-09-25 13:54:23 +0300

    Adding State Slices

--- reduxproject/src/store/index.js
+++ reduxproject/src/store/index.js
@@ -1,6 +1,29 @@
 import { createStore } from "redux";
 
-const counterReducer = (state = { counter: 0, showCounter: true }, action) => {
+import { createSlice } from "@reduxjs/toolkit";
+
+initialState = { counter: 0, showCounter: true };
+
+createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.amount;
+    },
+    toggle(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
+
+const counterReducer = (state = initialState, action) => {
   switch (action.type) {
     case "increment":
       return {
