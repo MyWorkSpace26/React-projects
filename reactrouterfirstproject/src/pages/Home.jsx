6a2a7a72318370e79a1a677c1e746beea852318d
Todo-List-Project <103925523+MyWorkSpace26@users.noreply.github.com>
2024-10-25 13:08:55 +0300

    Move between the pages via Link

--- reactrouterfirstproject/src/pages/Home.jsx
+++ reactrouterfirstproject/src/pages/Home.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
     <div>
       <h1>This is a Home Page</h1>
+      <p>
+        <Link to="/products">Go to Products Page</Link>
+      </p>
     </div>
   );
 };
