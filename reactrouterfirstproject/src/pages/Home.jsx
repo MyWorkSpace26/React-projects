573640f2479df8b6b797f58a7d616e06903cabfd
Todo-List-Project <103925523+MyWorkSpace26@users.noreply.github.com>
2024-10-25 13:32:38 +0300

    Layouts & Nested Routes

--- reactrouterfirstproject/src/pages/Home.jsx
+++ reactrouterfirstproject/src/pages/Home.jsx
@@ -1,12 +1,7 @@
-import { Link } from "react-router-dom";
-
 const HomePage = () => {
   return (
     <div>
       <h1>This is a Home Page</h1>
-      <p>
-        <Link to="/products">Go to Products Page</Link>
-      </p>
     </div>
   );
 };
