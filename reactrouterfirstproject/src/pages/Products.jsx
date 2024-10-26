d754145c43a12ef0c9933b28ffe3c54d5e32cd22
Todo-List-Project <103925523+MyWorkSpace26@users.noreply.github.com>
2024-10-26 11:26:40 +0300

    Navigate programmatically

--- reactrouterfirstproject/src/pages/Products.jsx
+++ reactrouterfirstproject/src/pages/Products.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const ProductsPage = () => {
+  const navigate = useNavigate();
+
+  const navigateHandler = () => {
+    navigate("/");
+  };
   return (
-    <div>
-      <h1>This is a Products Page</h1>
-    </div>
+    <>
+      <div>
+        <h1>This is a Products Page</h1>
+      </div>
+      <p>
+        <button onClick={navigateHandler}>Navigate</button>
+      </p>
+    </>
   );
 };
 
