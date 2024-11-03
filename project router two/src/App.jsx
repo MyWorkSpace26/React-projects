6a3b6561cfe745505e00cc5a7ada721f16366967
Todo-List-Project <103925523+MyWorkSpace26@users.noreply.github.com>
2024-11-03 16:41:34 +0300

    Data Fetching with a loader()

--- project router two/src/App.jsx
+++ project router two/src/App.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import RootLayout from "./pages/Root";
 import HomePage from "./pages/HomePage";
-import EventsPage from "./pages/EventsPage";
+import EventsPage from "./pages/Events";
 import EventDetailPage from "./pages/EventDetailPage";
 import NewEventPage from "./pages/NewEventPage";
 import EditEventPage from "./pages/EditEventPage";
@@ -24,6 +24,16 @@ function App() {
             {
               index: true,
               element: <EventsPage />,
+              loader: async () => {
+                const response = await fetch("http://localhost:8080/events");
+
+                if (!response.ok) {
+                  // ...
+                } else {
+                  const resData = await response.json();
+                  return resData.events;
+                }
+              },
             },
             {
               path: ":eventid",
