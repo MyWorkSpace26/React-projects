a9fca751e74c8b88569ff28fdaf5a7ccdfb1f4a3
Todo-List-Project <103925523+MyWorkSpace26@users.noreply.github.com>
2024-11-03 10:33:18 +0300

    second project router

---
+++ second project router/frontend/src/App.js
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router-dom";
+function App() {
+  /*  const router = createBrowserRouter([{
+    path: '/',
+    element: <HomePage />,
+    
+  }]) */
+
+  return <h1>amin</h1>;
+}
+
+export default App;
