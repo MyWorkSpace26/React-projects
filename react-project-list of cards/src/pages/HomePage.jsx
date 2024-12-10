73f5e6f48264d7c2bd20e855921e7819ca79ab50
Todo-List-Project <103925523+MyWorkSpace26@users.noreply.github.com>
2024-12-10 12:44:25 +0300

    Working with Home page

--- react-project-list of cards/src/pages/HomePage.jsx
+++ react-project-list of cards/src/pages/HomePage.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import styles from "../styles/HomePage.module.css";
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>Amin</h1>
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <h1 className={styles.title}>Добро пожаловать в Список Продуктов</h1>
+        <p className={styles.subtitle}>
+          Откройте для себя наши удивительные продукты
+        </p>
+      </header>
+      <main className={styles.main}>
+        <Link to="/products" className={styles.ctaButton}>
+          View All Products
+        </Link>
+      </main>
     </div>
   );
 };
