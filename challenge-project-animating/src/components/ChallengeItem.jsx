19f8efb8bf8ab96c4ed47a1ab2eb3d8d4e84ed48
Todo-List-Project <103925523+MyWorkSpace26@users.noreply.github.com>
2025-02-13 13:13:48 +0300

    Combining Animations With Layout Animations

--- challenge-project-animating/src/components/ChallengeItem.jsx
+++ challenge-project-animating/src/components/ChallengeItem.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 
 import { ChallengesContext } from "../store/challenges-context.jsx";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 export default function ChallengeItem({
   challenge,
   onViewDetails,
@@ -27,7 +27,7 @@ export default function ChallengeItem({
   }
 
   return (
-    <motion.li layout>
+    <motion.li layout exit={{ y: -30, opacity: 0 }}>
       <article className="challenge-item">
         <header>
           <img {...challenge.image} />
@@ -47,9 +47,7 @@ export default function ChallengeItem({
             <button onClick={onViewDetails}>
               View Details{" "}
               <motion.span
-                animate={{
-                  rotate: isExpanded ? 180 : 0,
-                }}
+                animate={{ rotate: isExpanded ? 180 : 0 }}
                 className="challenge-item-details-icon"
               >
                 &#9650;
@@ -57,13 +55,19 @@ export default function ChallengeItem({
             </button>
           </p>
 
-          {isExpanded && (
-            <div>
-              <p className="challenge-item-description">
-                {challenge.description}
-              </p>
-            </div>
-          )}
+          <AnimatePresence>
+            {isExpanded && (
+              <motion.div
+                initial={{ height: 0, opacity: 0 }}
+                animate={{ height: "auto", opacity: 1 }}
+                exit={{ height: 0, opacity: 0 }}
+              >
+                <p className="challenge-item-description">
+                  {challenge.description}
+                </p>
+              </motion.div>
+            )}
+          </AnimatePresence>
         </div>
       </article>
     </motion.li>
