c0ca0b4192d3c7d2554e16a45c6df80ee1e2b7f3
Todo-List-Project <103925523+MyWorkSpace26@users.noreply.github.com>
2025-02-26 21:01:23 +0300

    adding portfolio

---
+++ react-portfolio/src/components/articles/ArticleContactForm.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Article from "/src/components/wrappers/Article.jsx";
+
+function ArticleContactForm() {
+  return <Article className={`article-contact-form pb-2`}></Article>;
+}
+
+export default ArticleContactForm;
